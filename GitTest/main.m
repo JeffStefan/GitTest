78df37dd194286c8b4732eea7adc179ad2fb30ed
Jeff Stefan <jeff@jeff-stefan.com>
2015-07-29 10:05:17 -0400

    Initial Commit

---
+++ GitTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by Jeff Stefan on 7/29/15.
+//  Copyright (c) 2015 Induction Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
